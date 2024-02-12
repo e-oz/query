31888795c4c15f2da0872b913fef0632ac08567e
Evgeniy OZ <normandiggs@gmail.com>
2024-02-12 14:36:48 +0100

    attempt to help :)
    After ~1 hour of attempts, I couldn't find a solution to avoid `undefined` into the return type. I've modified this code to don't throw an exception when a required input is read, but it still brings `undefined` into the return type.
    I don't know how result of a query can be returned before the query, but it's just details of the implementation. Maybe there is some "dummy result" that is returned before the query is executed, I don't know, but then you could use that as a replacement for undefined.
    
    This commit is not supposed to be a PR, it is just part of the conversation.

--- packages/angular-query-experimental/src/create-base-query.ts
+++ packages/angular-query-experimental/src/create-base-query.ts
@@ -1,14 +1,6 @@
-import {
-  DestroyRef,
-  assertInInjectionContext,
-  computed,
-  effect,
-  inject,
-  signal,
-} from '@angular/core'
-import { notifyManager } from '@tanstack/query-core'
+import { assertInInjectionContext, DestroyRef, effect, inject, signal, } from '@angular/core'
+import { type DefaultedQueryObserverOptions, notifyManager, type QueryClient, type QueryKey, type QueryObserver, QueryObserverResult } from '@tanstack/query-core'
 import { signalProxy } from './signal-proxy'
-import type { QueryClient, QueryKey, QueryObserver } from '@tanstack/query-core'
 import type { CreateBaseQueryOptions, CreateBaseQueryResult } from './types'
 
 /**
@@ -38,48 +30,76 @@ export function createBaseQuery<
 
   /**
    * Signal that has the default options from query client applied
-   * computed() is used so signals can be inserted into the options
+   * effect() is used so signals can be inserted into the options
    * making it reactive. Wrapping options in a function ensures embedded expressions
    * are preserved and can keep being applied after signal changes
    */
-  const defaultedOptionsSignal = computed(() => {
+  const NON_INITIALIZED = Symbol('NonInitializedSignal');
+  type DefaultedOptions = DefaultedQueryObserverOptions<TQueryFnData, TError, TData, TQueryData, TQueryKey>;
+  const defaultedOptionsSignal = signal<DefaultedOptions | Symbol>(NON_INITIALIZED);
+
+  effect(() => {
     const defaultedOptions = queryClient.defaultQueryOptions(
       options(queryClient),
-    )
-    defaultedOptions._optimisticResults = 'optimistic'
-    return defaultedOptions
-  })
+    );
+    defaultedOptions._optimisticResults = 'optimistic';
+    defaultedOptionsSignal.set(defaultedOptions);
+  }, { allowSignalWrites: true });
 
-  const observer = new Observer<
+  let observer: QueryObserver<
     TQueryFnData,
     TError,
     TData,
     TQueryData,
     TQueryKey
-  >(queryClient, defaultedOptionsSignal())
+  > | undefined = undefined;
 
-  const resultSignal = signal(
-    observer.getOptimisticResult(defaultedOptionsSignal()),
-  )
+  const resultSignal = signal<QueryObserverResult<TData, TError> | undefined>(undefined);
 
   effect(
     () => {
       // Do not notify on updates because of changes in the options because
       // these changes should already be reflected in the optimistic result.
+
+      /**
+       * OZ:
+       * Code does the opposite (to comment) - it will update `resultSignal`
+       * every time signals inside `options` are modified, because of
+       * `computed()`/`effect()` used for `defaultedOptionsSignal`.
+       */
       const defaultedOptions = defaultedOptionsSignal()
-      observer.setOptions(defaultedOptions, {
-        listeners: false,
-      })
-      resultSignal.set(observer.getOptimisticResult(defaultedOptions))
+      if (defaultedOptions !== NON_INITIALIZED) {
+        const newOptions = defaultedOptions as DefaultedOptions;
+
+        if (!observer) {
+          observer = new Observer<
+            TQueryFnData,
+            TError,
+            TData,
+            TQueryData,
+            TQueryKey
+          >(queryClient, newOptions);
+
+          // observer.trackResult is not used as this optimization is not needed for Angular
+          const unsubscribe = observer.subscribe(
+            notifyManager.batchCalls((val) => resultSignal.set(val)),
+          )
+          destroyRef.onDestroy(unsubscribe)
+        }
+
+        observer.setOptions(newOptions, {
+          listeners: false,
+        })
+        resultSignal.set(observer.getOptimisticResult(newOptions))
+      }
     },
     { allowSignalWrites: true },
   )
 
-  // observer.trackResult is not used as this optimization is not needed for Angular
-  const unsubscribe = observer.subscribe(
-    notifyManager.batchCalls((val) => resultSignal.set(val)),
-  )
-  destroyRef.onDestroy(unsubscribe)
-
+  /**
+   * OZ:
+   * Looks like I messed with the types,
+   * or it's just because of possible `undefined`
+   */
   return signalProxy(resultSignal) as CreateBaseQueryResult<TData, TError>
 }
